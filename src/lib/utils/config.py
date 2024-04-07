8defa3f28bca45efcae41b14a05ed5059416df2d
hancee <hanceevilla@gmail.com>
2024-04-07 18:12:36 +0800

    Add config

---
+++ src/lib/utils/config.py
@@ -0,0 +1,3 @@
+import json
+from src.lib.utils.path_finder import CONFIG_PATH
+config = json.load(open(str(CONFIG_PATH)))
--- src/lib/utils/path_finder.py
+++ src/lib/utils/path_finder.py
@@ -1,3 +1,4 @@
 from pathlib import Path
 
 PROJECT_DIRECTORY = Path(__file__).parent.parent.parent.parent.resolve()
+CONFIG_PATH = PROJECT_DIRECTORY.joinpath(".config/config.json")
